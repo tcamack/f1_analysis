1e713e502ed808ef4496e328fde1990df26cbc39
tcamack <31225269+tcamack@users.noreply.github.com>
2021-10-09 17:11:37 -0600

    variable update

--- test/test.py
+++ test/test.py
@@ -4,12 +4,12 @@ from fastf1 import plotting
 
 plotting.setup_mpl()
 
-sochi_quali = ff1.get_session(2021, 'Monza', 'Q')
+test = ff1.get_session(2021, 'Turkey', 'Q')
 
-laps = sochi_quali.load_laps(with_telemetry = True)
+laps = test.load_laps(with_telemetry = True)
 
 # driver 1
-fast_driver_1 = laps.pick_driver('NOR').pick_fastest()
+fast_driver_1 = laps.pick_driver('HAM').pick_fastest()
 driver_1_car_data = fast_driver_1.get_car_data()
 vCar1 = driver_1_car_data['Speed']
 rpm1 = driver_1_car_data['RPM']
@@ -20,7 +20,7 @@ drs1 = driver_1_car_data['DRS']
 t1 = driver_1_car_data['Time']
 
 # driver 2
-fast_driver_2 = laps.pick_driver('RIC').pick_fastest()
+fast_driver_2 = laps.pick_driver('BOT').pick_fastest()
 driver_2_car_data = fast_driver_2.get_car_data()
 vCar2 = driver_2_car_data['Speed']
 rpm2 = driver_2_car_data['RPM']
@@ -32,11 +32,11 @@ t2 = driver_2_car_data['Time']
 
 fig, ax1 = plt.subplots()
 
-ax1.plot(t1, vCar1, label = 'Norris', color = 'orange')
-ax1.plot(t2, vCar2, label = 'Ricciardo', color = 'skyblue')
+ax1.plot(t1, vCar1, label = 'Hamilton', color = 'purple')
+ax1.plot(t2, vCar2, label = 'Bottas', color = 'skyblue')
 ax1.set_xlabel('Time')
 ax1.set_ylabel('Speed [Km/h]')
-ax1.set_title('Monza 2021 Qualifying Comparison')
+ax1.set_title('Turkey 2021 Qualifying Comparison')
 ax1.legend()
 
 # 2nd y axis
