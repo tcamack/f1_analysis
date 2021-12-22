15c7ce460429a79dde6e8e5a596a89bd26acc8ea
tcamack <31225269+tcamack@users.noreply.github.com>
2021-12-22 16:46:30 -0700

    Usability update.

--- f1_analysis.py
+++ f1_analysis.py
@@ -6,59 +6,97 @@ from fastf1 import plotting
 from matplotlib import pyplot as plt
 
 
-lap_number = 58
-cache = True
+def main():
+    """Primary function for data analysis and plotting."""
+    if cache:
+        if not os.path.exists('cache'):
+            os.mkdir('cache')
+        ff1.Cache.enable_cache('cache')
 
-if cache:
-    if not os.path.exists('cache'):
-        os.mkdir('cache')
-    ff1.Cache.enable_cache('cache')
+    def get_time(time):
+        """Reconstruct time in a readable format."""
+        minutes = 0
+        seconds, ms = str(time).split('.')
+        while int(seconds) > 60:
+            seconds = int(seconds) - 60
+            minutes += 1
 
-plotting.setup_mpl()
+        return f'{str(minutes).zfill(2)}:'\
+            f'{str(seconds).zfill(2)}.'\
+            f'{ms.ljust(3, "0")}'
 
-session = ff1.get_session(2021, 'Abu Dhabi', 'R')
-laps = session.load_laps(with_telemetry=True)
+    plotting.setup_mpl()
 
-# driver 1
-driver_1 = laps.pick_driver('HAM')
-driver_1_car_data = driver_1[driver_1['LapNumber'] ==
-                             lap_number].iloc[0].get_car_data()
-vCar1 = driver_1_car_data['Speed']
-rpm1 = driver_1_car_data['RPM']
-g1 = driver_1_car_data['nGear']
-throttle1 = driver_1_car_data['Throttle']
-brake1 = driver_1_car_data['Brake']
-drs1 = driver_1_car_data['DRS']
-t1 = driver_1_car_data['Time']
+    session = ff1.get_session(year, race, 'R')
+    laps = session.load_laps(with_telemetry=True)
 
-# driver 2
-driver_2 = laps.pick_driver('VER')
-driver_2_car_data = driver_2[driver_2['LapNumber'] ==
-                             lap_number].iloc[0].get_car_data()
-vCar2 = driver_2_car_data['Speed']
-rpm2 = driver_2_car_data['RPM']
-g2 = driver_2_car_data['nGear']
-throttle2 = driver_2_car_data['Throttle']
-brake2 = driver_2_car_data['Brake']
-drs2 = driver_2_car_data['DRS']
-t2 = driver_2_car_data['Time']
+    # Driver 1 delclaration and plot options.
+    driver_1 = laps.pick_driver(driver_1_initials)
+    driver_1_lap_data = driver_1[driver_1['LapNumber'] ==
+                                 lap_number].iloc[0]
+    driver_1_car_data = driver_1[driver_1['LapNumber'] ==
+                                 lap_number].iloc[0].get_car_data()
+    vCar1 = driver_1_car_data['Speed']
+    rpm1 = driver_1_car_data['RPM']
+    g1 = driver_1_car_data['nGear']
+    throttle1 = driver_1_car_data['Throttle']
+    brake1 = driver_1_car_data['Brake']
+    drs1 = driver_1_car_data['DRS']
+    t1 = driver_1_car_data['Time']
 
-fig, ax1 = plt.subplots()
+    # Driver 2 declaration and plot options.
+    driver_2 = laps.pick_driver(driver_2_initials)
+    driver_2_lap_data = driver_2[driver_2['LapNumber'] ==
+                                 lap_number].iloc[0]
+    driver_2_car_data = driver_2[driver_2['LapNumber'] ==
+                                 lap_number].iloc[0].get_car_data()
+    vCar2 = driver_2_car_data['Speed']
+    rpm2 = driver_2_car_data['RPM']
+    g2 = driver_2_car_data['nGear']
+    throttle2 = driver_2_car_data['Throttle']
+    brake2 = driver_2_car_data['Brake']
+    drs2 = driver_2_car_data['DRS']
+    t2 = driver_2_car_data['Time']
 
-ax1.plot(t1, vCar1, label='Hamilton', color='cyan')
-ax1.plot(t2, vCar2, label='Verstappen', color='blue')
-ax1.set_xlabel('Time')
-ax1.set_ylabel('Speed [Km/h]')
-ax1.set_title('Abu Dhabi 2021 Final Lap Comparison')
-ax1.legend()
+    driver_1_lap_time = driver_1_lap_data['LapTime'].total_seconds()
+    driver_2_lap_time = driver_2_lap_data['LapTime'].total_seconds()
 
-# 2nd y axis
-'''
-ax2 = ax1.twinx()
-ax2.plot(t1, throttle1, alpha=0.5, color='cyan')  # driver 1
-ax2.plot(t2, throttle2, alpha=0.5, color='blue')  # driver 2
-ax2.set_ylabel('Gear')
-'''
+    fig, ax1 = plt.subplots()
+
+    ax1.plot(t1,
+             vCar1,
+             label=f'{driver_1_initials} [{get_time(driver_1_lap_time)}]',
+             color=driver_1_color)
+    ax1.plot(t2,
+             vCar2,
+             label=f'{driver_2_initials} [{get_time(driver_2_lap_time)}]',
+             color=driver_2_color)
+    ax1.set_xlabel('Time [Seconds]')
+    ax1.set_ylabel('Speed [Km/h]')
+    ax1.set_title(f'{race} {year} Lap {lap_number} '
+                  f'{driver_1_initials} vs. {driver_2_initials}')
+    ax1.legend()
+
+    if second_y_axis:
+        ax2 = ax1.twinx()
+        ax2.plot(t1, drs1, linestyle='dashed', color=driver_1_color)
+        ax2.plot(t2, drs2, linestyle='dashed', color=driver_2_color)
+        ax2.set_ylabel('DRS')
+
+    plt.show()
+
 
 if __name__ == '__main__':
-    plt.show()
+    """Easy options, for advanced options edit main() function."""
+    year = 2021
+    race = 'Abu Dhabi'
+    lap_number = 58
+    driver_1_initials = 'HAM'
+    driver_1_color = 'cyan'
+    driver_2_initials = 'VER'
+    driver_2_color = 'blue'
+
+    cache = True
+    second_y_axis = False
+
+    main()
